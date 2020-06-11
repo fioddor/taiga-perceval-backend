c74c309a16296b782cd8ffb1b3c82047d165f074
fioddor <36269471+fioddor@users.noreply.github.com>
2020-06-11 19:03:31 +0200

    Pagination.
    Method for retrieval of data delivered by pages. Paginates to completion or to requested maximum.

--- min_taiga.py
+++ min_taiga.py
@@ -18,20 +18,25 @@
 #
 # Purpose: Minimal client in a library.
 #
-# Usage..: Call from other python program.
+# Usage..: 1. Instantiate object from other python program with either
+#             a) API token or
+#             b) user and password
+#          2. Only if you instantiated with user and password you need to (call) login.
 #
 # Design.: - Library with client class.
-#          - Reusable token.
+#          - Reusable user token.
 #          - Asynchronous =>
 #            - Close client sessions (pass them closed).
 #            - Signal server to close HTTP session.
 #            See https://stackoverflow.com/questions/10115126/python-requests-close-http-connection#15511852
+#          - Paginates to completion or to requested maximum.
 #
 # Author.: Fioddor Superconcentrado <fioddor@gmail.com>
 #
 #-----------------------------------------------------------------------------------------------------------------------------------------
 
-import requests        
+import requests
+from math import ceil
 
 
 
@@ -98,12 +103,60 @@ class TaigaMinClient():
      
      
     def rq(self, branch):
-        '''Most basic externally driven request handler.'''
+        '''Most basic externally driven request handler.
+        
+        :returns: a closed requests response. The full object is returned
+                  (whether successful or not) for further analysis. Only
+                  raises an exception if the client is not initiated.
+        '''
         if not self.headers:
-            self.login()
+            raise Exception( 'Client not yet initiated' )
         rs = requests.get( self.base_url+branch, headers=self.headers)
         rs.close()
         return rs
+     
+    
+    def rq_pages(self, branch, max_pages=None):
+        '''Generic request handler.
+         
+        :param max_pages: maximum number of pages to request. All pages, if
+            this argument is missing.
+        :returns: a list of Taiga JSON objects. Raises exceptions if anything
+            fails.
+        '''
+        if not self.headers:
+            raise Exception( 'Client not yet initiated.' )
+         
+        response = requests.get( self.base_url+branch, headers=self.headers )
+        if 200 != response.status_code:
+            raise Exception ( 'Http return code {} retrieving {}.'.format(response_status_code , self.base_url+branch) )
+        else:
+            output = response.json()
+             
+            if response.headers['x-paginated']:
+                max_taiga = ceil( int(response.headers['x-pagination-count'])
+                                / int(response.headers['x-paginated-by'])
+                                )
+                if max_pages:
+                    maximum = min([ max_pages , max_taiga ])
+                else:
+                    maximum = max_taiga
+                 
+                while int(response.headers['x-pagination-current']) < maximum:
+                    next_url = response.headers['X-Pagination-Next']
+                    response = requests.get( next_url , headers=self.headers )
+                    if 200 != response.status_code:
+                        print( response.headers )
+                        raise Exception( 'Http return code {} retrieving {}.'.format(response.status_code , next_url) )
+                    else:
+                        # print( response.headers )
+                        output.extend( response.json() )
+                        print( 'Debug.TaigaMinClient.rp_pages got yet {} items out of {}.'.
+                               format( len(output) , response.headers['x-pagination-count'] )
+                             )
+             
+            response.close()
+            return output
      
      
     def proj_stats(self, project):
@@ -116,9 +169,9 @@ class TaigaMinClient():
                 output[datum] = record[datum]
             return output
         else:
-            raise Exception( "TaigaClient failure: You shoudn't have happened!" )
-    
-    
+            raise Exception( "TaigaClient failure: This shoudn't have happened!" )
+     
+     
     def proj_issues_stats(self, project):
         '''Retrieve some basic issues_stats from the given project.'''
         response = self.rq( 'projects/{}/issues_stats'.format(project) )
@@ -129,5 +182,6 @@ class TaigaMinClient():
                 output[datum] = record[datum]
             return output
         else:
-            raise Exception( "TaigaClient failure: You shoudn't have happened!" )
+            raise Exception( "TaigaClient failure: This shoudn't have happened!" )
+
 
